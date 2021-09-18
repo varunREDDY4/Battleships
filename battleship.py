0f9d56249d56ad3da3052b90edac00860965c0b4
LaxmiNarayanaMurthyVemuri <murthyvemuri@msitprogram.net>
2021-09-18 15:03:06 +0530

    first commit

---
+++ battleship.py
@@ -0,0 +1,287 @@
+"""
+Battleship Project
+Name:
+Roll No:
+"""
+
+import battleship_tests as test
+
+project = "Battleship" # don't edit this
+
+### SIMULATION FUNCTIONS ###
+
+from tkinter import *
+import random
+
+EMPTY_UNCLICKED = 1
+SHIP_UNCLICKED = 2
+EMPTY_CLICKED = 3
+SHIP_CLICKED = 4
+
+
+'''
+makeModel(data)
+Parameters: dict mapping strs to values
+Returns: None
+'''
+def makeModel(data):
+    return
+
+
+'''
+makeView(data, userCanvas, compCanvas)
+Parameters: dict mapping strs to values ; Tkinter canvas ; Tkinter canvas
+Returns: None
+'''
+def makeView(data, userCanvas, compCanvas):
+    return
+
+
+'''
+keyPressed(data, events)
+Parameters: dict mapping strs to values ; key event object
+Returns: None
+'''
+def keyPressed(data, event):
+    pass
+
+
+'''
+mousePressed(data, event, board)
+Parameters: dict mapping strs to values ; mouse event object ; 2D list of ints
+Returns: None
+'''
+def mousePressed(data, event, board):
+    pass
+
+#### WEEK 1 ####
+
+'''
+emptyGrid(rows, cols)
+Parameters: int ; int
+Returns: 2D list of ints
+'''
+def emptyGrid(rows, cols):
+    return
+
+
+'''
+createShip()
+Parameters: no parameters
+Returns: 2D list of ints
+'''
+def createShip():
+    return
+
+
+'''
+checkShip(grid, ship)
+Parameters: 2D list of ints ; 2D list of ints
+Returns: bool
+'''
+def checkShip(grid, ship):
+    return
+
+
+'''
+addShips(grid, numShips)
+Parameters: 2D list of ints ; int
+Returns: 2D list of ints
+'''
+def addShips(grid, numShips):
+    return
+
+
+'''
+drawGrid(data, canvas, grid, showShips)
+Parameters: dict mapping strs to values ; Tkinter canvas ; 2D list of ints ; bool
+Returns: None
+'''
+def drawGrid(data, canvas, grid, showShips):
+    return
+
+
+### WEEK 2 ###
+
+'''
+isVertical(ship)
+Parameters: 2D list of ints
+Returns: bool
+'''
+def isVertical(ship):
+    return
+
+
+'''
+isHorizontal(ship)
+Parameters: 2D list of ints
+Returns: bool
+'''
+def isHorizontal(ship):
+    return
+
+
+'''
+getClickedCell(data, event)
+Parameters: dict mapping strs to values ; mouse event object
+Returns: list of ints
+'''
+def getClickedCell(data, event):
+    return
+
+
+'''
+drawShip(data, canvas, ship)
+Parameters: dict mapping strs to values ; Tkinter canvas; 2D list of ints
+Returns: None
+'''
+def drawShip(data, canvas, ship):
+    return
+
+
+'''
+shipIsValid(grid, ship)
+Parameters: 2D list of ints ; 2D list of ints
+Returns: bool
+'''
+def shipIsValid(grid, ship):
+    return
+
+
+'''
+placeShip(data)
+Parameters: dict mapping strs to values
+Returns: None
+'''
+def placeShip(data):
+    return
+
+
+'''
+clickUserBoard(data, row, col)
+Parameters: dict mapping strs to values ; int ; int
+Returns: None
+'''
+def clickUserBoard(data, row, col):
+    return
+
+
+### WEEK 3 ###
+
+'''
+updateBoard(data, board, row, col, player)
+Parameters: dict mapping strs to values ; 2D list of ints ; int ; int ; str
+Returns: None
+'''
+def updateBoard(data, board, row, col, player):
+    return
+
+
+'''
+runGameTurn(data, row, col)
+Parameters: dict mapping strs to values ; int ; int
+Returns: None
+'''
+def runGameTurn(data, row, col):
+    return
+
+
+'''
+getComputerGuess(board)
+Parameters: 2D list of ints
+Returns: list of ints
+'''
+def getComputerGuess(board):
+    return
+
+
+'''
+isGameOver(board)
+Parameters: 2D list of ints
+Returns: bool
+'''
+def isGameOver(board):
+    return
+
+
+'''
+drawGameOver(data, canvas)
+Parameters: dict mapping strs to values ; Tkinter canvas
+Returns: None
+'''
+def drawGameOver(data, canvas):
+    return
+
+
+### SIMULATION FRAMEWORK ###
+
+from tkinter import *
+
+def updateView(data, userCanvas, compCanvas):
+    userCanvas.delete(ALL)
+    compCanvas.delete(ALL)
+    makeView(data, userCanvas, compCanvas)
+    userCanvas.update()
+    compCanvas.update()
+
+def keyEventHandler(data, userCanvas, compCanvas, event):
+    keyPressed(data, event)
+    updateView(data, userCanvas, compCanvas)
+
+def mouseEventHandler(data, userCanvas, compCanvas, event, board):
+    mousePressed(data, event, board)
+    updateView(data, userCanvas, compCanvas)
+
+def runSimulation(w, h):
+    data = { }
+    makeModel(data)
+
+    root = Tk()
+    root.resizable(width=False, height=False) # prevents resizing window
+
+    # We need two canvases - one for the user, one for the computer
+    Label(root, text = "USER BOARD - click cells to place ships on your board.").pack()
+    userCanvas = Canvas(root, width=w, height=h)
+    userCanvas.configure(bd=0, highlightthickness=0)
+    userCanvas.pack()
+
+    compWindow = Toplevel(root)
+    compWindow.resizable(width=False, height=False) # prevents resizing window
+    Label(compWindow, text = "COMPUTER BOARD - click to make guesses. The computer will guess on your board.").pack()
+    compCanvas = Canvas(compWindow, width=w, height=h)
+    compCanvas.configure(bd=0, highlightthickness=0)
+    compCanvas.pack()
+
+    makeView(data, userCanvas, compCanvas)
+
+    root.bind("<Key>", lambda event : keyEventHandler(data, userCanvas, compCanvas, event))
+    compWindow.bind("<Key>", lambda event : keyEventHandler(data, userCanvas, compCanvas, event))
+    userCanvas.bind("<Button-1>", lambda event : mouseEventHandler(data, userCanvas, compCanvas, event, "user"))
+    compCanvas.bind("<Button-1>", lambda event : mouseEventHandler(data, userCanvas, compCanvas, event, "comp"))
+
+    updateView(data, userCanvas, compCanvas)
+
+    root.mainloop()
+
+
+### RUN CODE ###
+
+# This code runs the test cases to check your work
+if __name__ == "__main__":
+    print("\n" + "#"*15 + " WEEK 1 TESTS " +  "#" * 16 + "\n")
+    test.week1Tests()
+
+    ## Uncomment these for Week 2 ##
+    """
+    print("\n" + "#"*15 + " WEEK 2 TESTS " +  "#" * 16 + "\n")
+    test.week2Tests()
+    """
+
+    ## Uncomment these for Week 3 ##
+    """
+    print("\n" + "#"*15 + " WEEK 3 TESTS " +  "#" * 16 + "\n")
+    test.week3Tests()
+    """
+
+    ## Finally, run the simulation to test it manually ##
+    runSimulation(500, 500)
